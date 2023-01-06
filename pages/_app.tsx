3fdbf9d662558821a9f34dc61204fe14d1cfb7c2
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-01-07 02:15:20 +0530

    Added MUI Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,19 +3,24 @@ import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import store from "../store";
 import Head from "next/head";
+import { createTheme, ThemeProvider } from "@mui/system";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `"Montserrat", "Poppins", "Roboto", "Helvetica", "Arial", sans-serif`,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  // return <Component {...pageProps} />
-
   return (
     <Provider store={store}>
-      {/* <ThemeProvider theme={theme}> */}
-      <Head>
-        <title>Team Concoders</title>
-      </Head>
-      <Component {...pageProps} />
-      {/* <ToastContainer position="bottom-center" theme="light" /> */}
-      {/* </ThemeProvider> */}
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Team Concoders</title>
+        </Head>
+        <Component {...pageProps} />
+        {/* <ToastContainer position="bottom-center" theme="light" /> */}
+      </ThemeProvider>
     </Provider>
   );
 }
