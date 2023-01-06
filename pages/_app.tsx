a81b51ccb859684735381839879dc57f71749564
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-01-07 02:13:07 +0530

    Added Redux Store to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+
+  return (
+    <Provider store={store}>
+      {/* <ThemeProvider theme={theme}> */}
+      <Head>
+        <title>Team Concoders</title>
+      </Head>
+      <Component {...pageProps} />
+      {/* <ToastContainer position="bottom-center" theme="light" /> */}
+      {/* </ThemeProvider> */}
+    </Provider>
+  );
 }
