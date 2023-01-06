1e1e577c9ce61a553b0d9ae0d8fdf036787fdc45
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-01-07 01:45:06 +0530

    Added Wallet Connection Code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,36 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
+import { Button } from "@mui/material";
+import { detectConcordiumProvider } from "@concordium/browser-wallet-api-helpers";
+import type { WalletApi } from "@concordium/browser-wallet-api-helpers";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [state, setState] = useState<{
+    provider?: WalletApi;
+    account?: string;
+  }>({
+    provider: undefined,
+    account: undefined,
+  });
+
+  const onConnectClick: React.MouseEventHandler<HTMLButtonElement> = async (
+    event
+  ) => {
+    try {
+      const provider = await detectConcordiumProvider();
+      const accountAddr = await provider.connect();
+      setState({ provider, account: accountAddr });
+
+      detectConcordiumProvider().then((provider) => {});
+    } catch (err) {
+      console.error(`Error connecting to wallet: ${err}`);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -12,6 +38,10 @@ export default function Home() {
       </Head>
       <div>
         <h1>Team Concoders</h1>
+
+        <button onClick={onConnectClick}>Connect</button>
+
+        <p>Account Address: {state.account}</p>
       </div>
     </>
   );
