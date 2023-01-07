07b784eb313d99877c361ac63e4140d799280358
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-01-07 17:30:39 +0530

    Added Cards to Home

---
+++ components/cards/item-card.tsx
@@ -0,0 +1,48 @@
+import { Button, Grid } from "@mui/material";
+import classes from "./card.module.scss";
+
+interface ItemCardProps {
+  name: string;
+  price: number;
+  img_url: string;
+  creator: string;
+  owners: string[];
+  className?: string;
+}
+
+const ItemCard: React.FC<ItemCardProps> = ({
+  name,
+  price,
+  img_url,
+  creator,
+  owners,
+  className,
+}) => {
+  return (
+    <div className={classes.card}>
+      <img src={img_url} alt="Item Image" />
+
+      <p>Dark Elixer</p>
+
+      <p>
+        Buy from <span>CCD {price}</span>
+      </p>
+
+      <Grid container gap={1}>
+        <Grid item flexGrow={1}>
+          <Button variant="contained" fullWidth>
+            BUY
+          </Button>
+        </Grid>
+
+        <Grid item flexGrow={1}>
+          <Button variant="contained" fullWidth>
+            SELL
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ItemCard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import { Button } from "@mui/material";
+import { Button, Container, Grid } from "@mui/material";
 import { detectConcordiumProvider } from "@concordium/browser-wallet-api-helpers";
 import type { WalletApi } from "@concordium/browser-wallet-api-helpers";
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { walletActions } from "../store/wallet-store";
 import NavBarHome from "../components/app/navbar/navbar-main";
+import ItemCard from "../components/cards/item-card";
 
 export default function Home() {
   const dispatch = useAppDispatch();
@@ -32,6 +33,14 @@ export default function Home() {
     }
   };
 
+  const items = Array(5).fill({
+    name: "Dark Elixer",
+    price: 100,
+    img_url: "https://wallpapercave.com/wp/wp2424020.jpg",
+    creator: "4KuyV4gYjp5vwsPm33NPPknxzUv8AG8uvGK1NLLfzjW8KtcAtQ",
+    owners: ["4KuyV4gYjp5vwsPm33NPPknxzUv8AG8uvGK1NLLfzjW8KtcAtQ"],
+  });
+
   return (
     <>
       <Head>
@@ -40,13 +49,23 @@ export default function Home() {
 
       <NavBarHome />
 
-      <div>
+      <Container>
         <h1>Team Concoders</h1>
 
         <button onClick={onConnectClick}>Connect</button>
 
         <p>Account Address: {walletState.account}</p>
-      </div>
+
+        <Grid container gap={3} justifyContent="center">
+          {items.map((item, i) => {
+            return (
+              <Grid item key={i}>
+                <ItemCard {...item} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Container>
     </>
   );
 }
